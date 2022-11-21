80aa9d80e3b6d924225ef4642b84171e7cb19f45
Coding-64-Bit <117987625+Coding-64-Bit@users.noreply.github.com>
2022-11-21 20:57:54 +0000

    Commit

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import pygame, sys
 from settings import *
 
 class Game:
-    def __init__(self)
-    pygame.init()
-    self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
-    self.clock = pygame.time.Clock()
+    def __init__(self):
+
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
+        self.clock = pygame.time.Clock()
 
     def run(self):
         while True:
@@ -19,10 +20,5 @@ class Game:
             self.clock.tick(FPS)
 
 if __name__ == '__main__':
-    
-
-
-        
-
-
-
+    game = Game()
+    game.run()
